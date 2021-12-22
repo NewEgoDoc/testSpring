1da40f1ca405fd7f199c0b1fed08e9934892947a
Juhan <juhan211@naver.com>
2021-12-22 21:39:11 +0900

    spring 기본의 복습!!!!

---
+++ src/test/java/hello/testSpring/TestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.testSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
